c2e8ae52402302319dbc87bb30cbd8d68bd5eb2f
Jennifer <jmarthagodfrey@gmail.com>
2024-05-02 13:50:50 -0600

    create dataLoader, create jobpage

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,7 @@ import MainLayout from './layouts/MainLayout';
 import NotFoundPage from './pages/NotFoundPage';
 import HomePage from './pages/HomePage';
 import JobsPage from './pages/JobsPage';
+import JobPage, { jobLoader } from './pages/JobPage';
 
 
 const router = createBrowserRouter(
@@ -16,6 +17,7 @@ const router = createBrowserRouter(
     <Route path='/' element={<MainLayout />}>
       <Route index element={<HomePage />} />
       <Route path='/jobs' element={<JobsPage />} />
+      <Route path='/jobs/:id' element={<JobPage />} loader={jobLoader} />
       <Route path='*' element={<NotFoundPage />} />
     </Route>
     )
